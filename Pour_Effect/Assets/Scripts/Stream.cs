b4180bf370729457acc08a3d251ff4bea9f95c78
sakellaro <sakella.pan@gmail.com>
2020-12-21 01:21:44 +0200

    Add Pour Effect Project

---
+++ Pour_Effect/Assets/Scripts/Stream.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stream : MonoBehaviour
+{
+    private LineRenderer lineRenderer = null;
+
+    private Vector3 targetPosition = Vector3.zero ;
+    private ParticleSystem splashPaticle = null;
+
+    private Coroutine poorRoutine = null;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        splashPaticle = GetComponent<ParticleSystem>();
+    }
+
+    private void Start()
+    {
+        MoveToPosition(0, transform.position);
+        MoveToPosition(1, transform.position);
+    }
+
+    public void Begin()
+    {
+        StartCoroutine(UpdateParticle());
+        poorRoutine = StartCoroutine(BeginPour());
+    }
+
+    private IEnumerator BeginPour()
+    {
+        while (gameObject.activeSelf)
+        {
+            targetPosition=FindEndPoint();
+
+            MoveToPosition(0, transform.position);
+            AnimateToPosition(1, targetPosition);
+
+            yield return null;
+        }
+
+    }
+
+    public void End()
+    {
+        StopCoroutine(poorRoutine);
+        poorRoutine = StartCoroutine(EndPour());
+    }
+
+    private IEnumerator EndPour()
+    {
+        while (!HasReachedPosition(0, targetPosition))
+        {
+            AnimateToPosition(0, targetPosition);
+            AnimateToPosition(1, targetPosition);
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+
+    private Vector3 FindEndPoint()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        Physics.Raycast(ray, out hit, 2.0f);
+        Vector3 endPoint = hit.collider ? hit.point : ray.GetPoint(2.0f);
+        
+        return endPoint;
+    }
+
+    private void MoveToPosition(int index, Vector3 targetPosition)
+    {
+        lineRenderer.SetPosition(index, targetPosition);
+    }
+
+    private void AnimateToPosition(int index, Vector3 targetPosition)
+    {
+        Vector3 currentPoint = lineRenderer.GetPosition(index);
+        Vector3 newPosition = Vector3.MoveTowards(currentPoint, targetPosition, Time.deltaTime * 1.75f);
+        lineRenderer.SetPosition(index, newPosition);
+    }
+
+    private bool HasReachedPosition(int index, Vector3 targetPosition)
+    {
+        Vector3 currentPosition = lineRenderer.GetPosition(index);
+        return currentPosition==targetPosition;
+    }
+
+    private IEnumerator UpdateParticle()
+    {
+        while (gameObject.activeSelf)
+        {
+            splashPaticle.gameObject.transform.position = targetPosition;
+            bool inHitting = HasReachedPosition(1, targetPosition);
+            splashPaticle.gameObject.SetActive(inHitting);
+            yield return null;
+        }
+    }
+}
